503a6a3ccc74b0a0a6356e7ac093f5f9febbfa57
Aditya Kamath <cs11b001@iith.ac.in>
2016-01-02 19:01:24 -0500

    Fixed building of dependent libraries

--- libsrc/compile.sh
+++ libsrc/compile.sh
@@ -9,6 +9,7 @@ mkdir libs
 git submodule update --init --recursive
 # Build Heros
 echo "Building Heros"
+cp settings/heros/ant.settings heros/
 cd heros
 ant
 cp heros-trunk.jar ../libs/
@@ -17,6 +18,7 @@ echo "Done building Heros"
 
 # Build Jasmin
 echo "Building Jasmin"
+cp settings/jasmin/ant.settings jasmin/
 cd jasmin
 #git pull
 ant barebones
@@ -28,6 +30,7 @@ echo "Done building Jasmin"
 
 # Build Soot
 echo "Building Soot"
+cp settings/soot/ant.settings soot/
 cd soot
 ant clean
 ant barebones
@@ -38,6 +41,8 @@ echo "Done building soot"
 
 # Building Booster
 echo "Building TamiFlex-Booster"
+cp settings/booster/ant.settings tamiflex/Booster/
+cp settings/booster/build.xml tamiflex/Booster/
 cd ./tamiflex/Booster
 rm lib/soot-trunk.jar
 rm lib/booster-trunk.jar
