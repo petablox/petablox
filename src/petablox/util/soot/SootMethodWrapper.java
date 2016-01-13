ba200d7b7f6fb38300d3ed4b0c6fbfdfa1984b5a
Aditya Kamath <cs11b001@iith.ac.in>
2016-01-13 16:14:45 -0500

    Added additional fields to SootMethodWrapper for book-keeping

--- src/petablox/util/soot/SootMethodWrapper.java
+++ src/petablox/util/soot/SootMethodWrapper.java
@@ -12,18 +12,35 @@ public class SootMethodWrapper {
     private SootMethod m;
     private int preOrder;
     private int postOrder;
+    private int index;
+    public boolean processed;
 
     public SootMethodWrapper(){
         m = null;
         preOrder = -1;
         postOrder = -1;
+        index = -1;
+        processed = false;
     }
     public SootMethodWrapper(SootMethod m){
         this();
         this.m = m;
     }
+
+    public SootMethodWrapper(SootMethod m, int pre, int index){
+        this.m = m;
+        this.preOrder = pre;
+        this.postOrder = -1;
+        this.index = index;
+        processed = false;
+    }
+
     public SootMethod getSootMethod(){
         return m;
+    }
+
+    public int getIndex(){
+        return index;
     }
 
     public int getPreOrder(){
