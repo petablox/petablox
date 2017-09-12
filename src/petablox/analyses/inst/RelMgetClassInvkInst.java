0f03d79a6c6a0ac90ec62105b6fc5470d7729b65
Kihong Heo <kheo@cis.upenn.edu>
2017-09-12 18:42:11 +0000

    [bug fix] fix compile errors

--- src/petablox/analyses/inst/RelMgetClassInvkInst.java
+++ src/petablox/analyses/inst/RelMgetClassInvkInst.java
@@ -3,6 +3,8 @@ package petablox.analyses.inst;
 import soot.SootClass;
 import soot.SootMethod;
 import soot.Unit;
+import soot.jimple.internal.JInvokeStmt;
+
 import petablox.analyses.invk.DomI;
 import petablox.analyses.method.DomM;
 import petablox.program.visitors.IInvokeInstVisitor;
@@ -11,42 +13,42 @@ import petablox.project.analyses.ProgramRel;
 import petablox.util.soot.SootUtilities;
 
 /**
- * Relation containing each tuple (m,i) such that method m contains a
- * statement i invoking instance method getClass() defined in class
- * java.lang.Object.
+ * Relation containing each tuple (m,i) such that method m contains a statement i invoking instance
+ * method getClass() defined in class java.lang.Object.
  *
  * @author Mayur Naik (mhn@cs.stanford.edu)
  */
-@Petablox(
-    name = "MgetClassInvkInst",
-    sign = "M0,I0:M0xI0"
-)
-public class RelMgetClassInvkInst extends ProgramRel
-        implements IInvokeInstVisitor {
+@Petablox(name = "MgetClassInvkInst", sign = "M0,I0:M0xI0")
+public class RelMgetClassInvkInst extends ProgramRel implements IInvokeInstVisitor {
     private DomM domM;
     private DomI domI;
     private SootMethod ctnrMethod;
+
     public void init() {
         domM = (DomM) doms[0];
         domI = (DomI) doms[1];
     }
-    public void visit(SootClass c) { }
+
+    public void visit(SootClass c) {}
+
     public void visit(SootMethod m) {
         ctnrMethod = m;
     }
+
+    public void visit(JInvokeStmt s) {}
+
     public void visitInvokeInst(Unit q) {
-    	if(SootUtilities.isInvoke(q)){
-    		SootMethod meth = SootUtilities.getInvokeExpr(q).getMethod();
-    		if (meth.getName().equals("getClass") &&
-    	        meth.getBytecodeSignature().contains("()Ljava/lang/Class;") &&
-    	        meth.getDeclaringClass().getName().equals("java.lang.Object")) {
-    			int mIdx = domM.indexOf(ctnrMethod);
+        if (SootUtilities.isInvoke(q)) {
+            SootMethod meth = SootUtilities.getInvokeExpr(q).getMethod();
+            if (meth.getName().equals("getClass")
+                    && meth.getBytecodeSignature().contains("()Ljava/lang/Class;")
+                    && meth.getDeclaringClass().getName().equals("java.lang.Object")) {
+                int mIdx = domM.indexOf(ctnrMethod);
                 assert (mIdx >= 0);
                 int iIdx = domI.indexOf(q);
                 assert (iIdx >= 0);
                 add(mIdx, iIdx);
-    		}
-    		
-    	}
+                    }
+        }
     }
 }
--- src/petablox/analyses/invk/DomI.java
+++ src/petablox/analyses/invk/DomI.java
@@ -1,33 +1,32 @@
 package petablox.analyses.invk;
 
-import soot.SootClass;
-import soot.SootMethod;
-import soot.Unit;
-import soot.tagkit.SourceFileTag;
-import soot.tagkit.Tag;
-import soot.jimple.DynamicInvokeExpr;
-import soot.jimple.InvokeExpr;
-import soot.jimple.internal.JAssignStmt;
-import soot.tagkit.LineNumberTag;
-
 import java.util.List;
-
 import petablox.analyses.method.DomM;
 import petablox.program.visitors.IInvokeInstVisitor;
-import petablox.project.Petablox;
 import petablox.project.ClassicProject;
 import petablox.project.Config;
+import petablox.project.Petablox;
 import petablox.project.analyses.ProgramDom;
 import petablox.util.Utils;
 import petablox.util.soot.SootUtilities;
-
+import soot.SootClass;
+import soot.SootMethod;
+import soot.Unit;
+import soot.jimple.DynamicInvokeExpr;
+import soot.jimple.InvokeExpr;
+import soot.jimple.internal.JInvokeStmt;
+import soot.tagkit.SourceFileTag;
+import soot.tagkit.Tag;
 
 /**
  * Domain of method invocation quads.
- * 
+ *
  * @author Mayur Naik (mhn@cs.stanford.edu)
  */
-@Petablox(name = "I", consumes = { "M", "PM" })
+@Petablox(
+  name = "I",
+  consumes = {"M", "PM"}
+)
 public class DomI extends ProgramDom<Unit> implements IInvokeInstVisitor {
     protected DomM domM;
 
@@ -37,10 +36,13 @@ public class DomI extends ProgramDom<Unit> implements IInvokeInstVisitor {
     }
 
     @Override
-    public void visit(SootClass c) { }
+    public void visit(SootClass c) {}
 
     @Override
-    public void visit(SootMethod m) { }
+    public void visit(SootMethod m) {}
+
+    @Override
+    public void visit(JInvokeStmt s) {}
 
     @Override
     public void visitInvokeInst(Unit u) {
@@ -49,51 +51,48 @@ public class DomI extends ProgramDom<Unit> implements IInvokeInstVisitor {
 
     @Override
     public String toUniqueString(Unit u) {
-        return SootUtilities.toByteLocStr(u);                      
+        return SootUtilities.toByteLocStr(u);
     }
 
     @Override
     public String toFIString(Unit u) {
-    	StringBuilder sb = new StringBuilder();
-    	boolean printId = Utils.buildBoolProperty("petablox.printrel.printID", false);
-    	if(printId) sb.append("(" + indexOf(u) + ")");
-    	InvokeExpr ie = SootUtilities.getInvokeExpr(u);
-    	if(ie instanceof DynamicInvokeExpr){
-    		return "";
-    	}
-    	if (SootUtilities.isInterfaceInvoke(u))
-    		sb.append("INTERFACEINVK:");
-    	else if (SootUtilities.isVirtualInvoke(u))
-    		sb.append("VIRTUALINVK:");
-    	else if (SootUtilities.isStaticInvoke(u))
-    		sb.append("STATICINVK:");
-    	else if (SootUtilities.isInstanceInvoke(u))
-    		sb.append("SPECINVK:");
-    	SootMethod m = SootUtilities.getInvokeExpr(u).getMethod();
-    	sb.append(m.getName() + "@" + m.getDeclaringClass().getName());
-    	return sb.toString();
+        StringBuilder sb = new StringBuilder();
+        boolean printId = Utils.buildBoolProperty("petablox.printrel.printID", false);
+        if (printId) sb.append("(" + indexOf(u) + ")");
+        InvokeExpr ie = SootUtilities.getInvokeExpr(u);
+        if (ie instanceof DynamicInvokeExpr) {
+            return "";
+        }
+        if (SootUtilities.isInterfaceInvoke(u)) sb.append("INTERFACEINVK:");
+        else if (SootUtilities.isVirtualInvoke(u)) sb.append("VIRTUALINVK:");
+        else if (SootUtilities.isStaticInvoke(u)) sb.append("STATICINVK:");
+        else if (SootUtilities.isInstanceInvoke(u)) sb.append("SPECINVK:");
+        SootMethod m = SootUtilities.getInvokeExpr(u).getMethod();
+        sb.append(m.getName() + "@" + m.getDeclaringClass().getName());
+        return sb.toString();
     }
-    
+
     @Override
     public String toXMLAttrsString(Unit u) {
-    	// this piece of code seems to be copied from DomE.java
-    	// which does not make much sense here
+        // this piece of code seems to be copied from DomE.java
+        // which does not make much sense here
         SootMethod m = SootUtilities.getMethod(u);
-        //JAssignStmt as = (JAssignStmt)u;
-        //String file = ((SourceFileTag)m.getDeclaringClass().getTags().get(0)).getSourceFile();              
+        // JAssignStmt as = (JAssignStmt)u;
+        // String file = ((SourceFileTag)m.getDeclaringClass().getTags().get(0)).getSourceFile();
         List<Tag> tags = m.getDeclaringClass().getTags();
         String file = null;
-        for(Tag x : tags) {
-        	if( x instanceof SourceFileTag) {
-        		file = ((SourceFileTag)x).getSourceFile();
-        		break;
-        	}
+        for (Tag x : tags) {
+            if (x instanceof SourceFileTag) {
+                file = ((SourceFileTag) x).getSourceFile();
+                break;
+            }
         }
-        //int line = ((LineNumberTag)u.getTag("LineNumberTag")).getLineNumber();
+        // int line = ((LineNumberTag)u.getTag("LineNumberTag")).getLineNumber();
         int mIdx = domM.indexOf(m);
-		return "file=\"" + file + "\" " + "Mid=\"M" + mIdx + "\" ";
-        //return "file=\"" + file + "\" " + "line=\"" + line + "\" " +
+        return "file=\"" + file + "\" " + "Mid=\"M" + mIdx + "\" ";
+        // return "file=\"" + file + "\" " + "line=\"" + line + "\" " +
         //   "Mid=\"M" + mIdx + "\"" ;
-            //" rdwr=\"" + ((SootUtilities.isFieldStore(as) || SootUtilities.isStaticPut(as) || SootUtilities.isStoreInst(as)) ? "Wr" : "Rd") + "\"";
+        // " rdwr=\"" + ((SootUtilities.isFieldStore(as) || SootUtilities.isStaticPut(as) ||
+        // SootUtilities.isStoreInst(as)) ? "Wr" : "Rd") + "\"";
     }
 }
